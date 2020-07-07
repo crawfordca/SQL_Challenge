981a3497e49443bc01089cdf0d99c531062e01b2
crawfordca <cameo.crawford@gmail.com>
2020-07-07 18:28:56 -0500

    Update schema and ERD

--- EmplolyeeSQL/schema.sql
+++ EmplolyeeSQL/schema.sql
@@ -1,38 +1,38 @@
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS titles;
-DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS dept_emp;
 DROP TABLE IF EXISTS dept_manager;
 DROP TABLE IF EXISTS salaries;
-
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS titles;
 
 CREATE TABLE departments (
-	id SERIAL PRIMARY KEY,
-	dept_no VARCHAR not null,
+	id SERIAL,
+	dept_no VARCHAR NOT null PRIMARY KEY,
 	dept_name VARCHAR
 );
 
 CREATE TABLE titles (
-	id SERIAL PRIMARY KEY,	
-	title_id VARCHAR NOT NULL,
+	id SERIAL,	
+	title_id VARCHAR NOT NULL PRIMARY KEY,
 	title VARCHAR
 );
 
 CREATE TABLE employees (
-	id SERIAL PRIMARY KEY,
-	emp_no INTEGER NOT NULL,
+	id SERIAL,
+	emp_no VARCHAR NOT NULL,
 	emp_title_id VARCHAR REFERENCES titles(title_id),
 	birth_date DATE,
 	first_name VARCHAR,
 	last_name VARCHAR,
 	sex TEXT,
-	hire_date DATE
+	hire_date DATE,
+	PRIMARY KEY (emp_no)
 );
 
 
 CREATE TABLE dept_emp (
 	id SERIAL PRIMARY KEY,
-	emp_no INTEGER NOT NULL,
+	emp_no VARCHAR NOT NULL,
 	dept_no VARCHAR NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
@@ -40,14 +40,14 @@ CREATE TABLE dept_emp (
 CREATE TABLE dept_manager (
 	id SERIAL PRIMARY KEY,
 	dept_no VARCHAR NOT NULL,
-	emp_no INTEGER NOT NULL,
+	emp_no VARCHAR NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE salaries (
 	id SERIAL PRIMARY KEY,
-	emp_no INTEGER NOT NULL,
+	emp_no VARCHAR NOT NULL,
 	salary INTEGER,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
