fc697623c61ce3459aafda3fba941ce67785f2d8
crawfordca <cameo.crawford@gmail.com>
2020-07-07 14:23:52 -0500

    Update schema.sql
    Update primary and foreign keys

--- EmplolyeeSQL/schema.sql
+++ EmplolyeeSQL/schema.sql
@@ -1,49 +1,55 @@
 DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS dept_emp;
 DROP TABLE IF EXISTS dept_manager;
-DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS salaries;
-DROP TABLE IF EXISTS titles;
 
 
 CREATE TABLE departments (
-	id serial PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	dept_no VARCHAR not null,
 	dept_name VARCHAR
 );
 
-CREATE TABLE dept_emp (
-	id serial PRIMARY KEY,
-	emp_no INTEGER,
-	dept_no VARCHAR
-);
-
-CREATE TABLE dept_manager (
-	id serial PRIMARY KEY
-	dept_no VARCHAR,
-	emp_no INTEGER
+CREATE TABLE titles (
+	id SERIAL PRIMARY KEY,	
+	title_id VARCHAR NOT NULL,
+	title VARCHAR
 );
 
 CREATE TABLE employees (
-	id serial PRIMARY KEY,
-	emp_no INTEGER,
-	emp_title_id VARCHAR,
+	id SERIAL PRIMARY KEY,
+	emp_no INTEGER NOT NULL,
+	emp_title_id VARCHAR NOT NULL,
 	birth_date DATE,
 	first_name VARCHAR,
 	last_name VARCHAR,
 	sex TEXT,
-	hire_date DATE
+	hire_date DATE,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+
+CREATE TABLE dept_emp (
+	id SERIAL PRIMARY KEY,
+	emp_no INTEGER NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_manager (
+	id SERIAL PRIMARY KEY,
+	dept_no VARCHAR NOT NULL,
+	emp_no INTEGER NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE salaries (
-	id serial PRIMARY KEY,
-	emp_no INTEGER,
-	salary INTEGER
-);
-
-CREATE TABLE titles (
-	id serial PRIMARY KEY,	
-	title_id VARCHAR,
-	title VARCHAR
+	id SERIAL PRIMARY KEY,
+	emp_no INTEGER NOT NULL,
+	salary INTEGER,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
